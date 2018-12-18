b36eccf15f40e9181bd23f9a70a23fa9c52f71d4
sberton <sylvain.berton.pro@gmail.com>
2018-12-18 08:09:32 -0800

    first commit

---
+++ src/main/java/analytics/App.java
@@ -0,0 +1,13 @@
+package analytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
